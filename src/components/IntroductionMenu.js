74c68427657ea0202852c416dd5df9984d4d6392
Ismatsan <ismat7300@gmail.com>
2022-08-10 23:12:39 +0600

    feat: added_div_to_IntroductionMenu

--- src/components/IntroductionMenu.js
+++ src/components/IntroductionMenu.js
@@ -6,54 +6,56 @@ import Title from "./Title";
 
 const popular_posts = [
     {
-        img:'https://www.w3schools.com/w3images/workshop.jpg',
-        title:'Lorem',
-        desc:'Sed mattis nunc'
+        img: 'https://www.w3schools.com/w3images/workshop.jpg',
+        title: 'Lorem',
+        desc: 'Sed mattis nunc'
     },
     {
-        img:'https://www.w3schools.com/w3images/gondol.jpg',
-        title:'Ipsum',
-        desc:'Praes tinci sed'
+        img: 'https://www.w3schools.com/w3images/gondol.jpg',
+        title: 'Ipsum',
+        desc: 'Praes tinci sed'
     },
     {
-        img:'https://www.w3schools.com/w3images/skies.jpg',
-        title:'Dorum',
-        desc:'Ultricies congue'
+        img: 'https://www.w3schools.com/w3images/skies.jpg',
+        title: 'Dorum',
+        desc: 'Ultricies congue'
     },
     {
-        img:'https://www.w3schools.com/w3images/rock.jpg',
-        title:'Mingsum',
-        desc:'Lorem ipsum dipsum'
+        img: 'https://www.w3schools.com/w3images/rock.jpg',
+        title: 'Mingsum',
+        desc: 'Lorem ipsum dipsum'
     }
 ]
 
-const IntroductionMenu = () =>{
-    return(
-       <div className="w3-col l4">
-           <About/>
-           <Title/>
-           <PopularPosts
-               img ={popular_posts[0].img}
-               title ={popular_posts[0].title}
-               desc ={popular_posts[0].desc}
-           />
-           <PopularPosts
-               img ={popular_posts[1].img}
-               title ={popular_posts[1].title}
-               desc ={popular_posts[1].desc}
-           />
-           <PopularPosts
-               img ={popular_posts[2].img}
-               title ={popular_posts[2].title}
-               desc ={popular_posts[2].desc}
-           />
-           <PopularPosts
-               img ={popular_posts[3].img}
-               title ={popular_posts[3].title}
-               desc ={popular_posts[3].desc}
-           />
-           <Tags/>
-       </div>
+const IntroductionMenu = () => {
+    return (
+        <div className="w3-col l4">
+            <About/>
+            <div className="w3-card w3-margin">
+                <Title/>
+                <PopularPosts
+                    img={popular_posts[0].img}
+                    title={popular_posts[0].title}
+                    desc={popular_posts[0].desc}
+                />
+                <PopularPosts
+                    img={popular_posts[1].img}
+                    title={popular_posts[1].title}
+                    desc={popular_posts[1].desc}
+                />
+                <PopularPosts
+                    img={popular_posts[2].img}
+                    title={popular_posts[2].title}
+                    desc={popular_posts[2].desc}
+                />
+                <PopularPosts
+                    img={popular_posts[3].img}
+                    title={popular_posts[3].title}
+                    desc={popular_posts[3].desc}
+                />
+            </div>
+            <Tags/>
+        </div>
     );
 }
 
--- src/components/PopularPosts.js
+++ src/components/PopularPosts.js
@@ -2,7 +2,6 @@ import React from "react";
 
 const PopularPosts = (props) => {
     return (
-        <div className="w3-card w3-margin">
             <ul className="w3-ul w3-hoverable w3-white">
                 <li className="w3-padding-16">
                     <img src={props.img} alt="Image" className="w3-left w3-margin-right" style={{width: "50px"}}/>
@@ -11,7 +10,6 @@ const PopularPosts = (props) => {
                     <span>{props.desc}</span>
                 </li>
             </ul>
-        </div>
     );
 }
 
