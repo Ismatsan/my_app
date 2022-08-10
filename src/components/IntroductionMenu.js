68d696291408a1d6b96921b3202782ca52c2ce4e
Ismatsan <ismat7300@gmail.com>
2022-08-10 23:05:13 +0600

    feat: added_ component_Title.js_and_component_IntroductionMenu_made_with_props

--- src/components/IntroductionMenu.js
+++ src/components/IntroductionMenu.js
@@ -2,15 +2,59 @@ import React from "react";
 import About from "./About";
 import PopularPosts from "./PopularPosts";
 import Tags from "./Tags";
+import Title from "./Title";
+
+const popular_posts = [
+    {
+        img:'https://www.w3schools.com/w3images/workshop.jpg',
+        title:'Lorem',
+        desc:'Sed mattis nunc'
+    },
+    {
+        img:'https://www.w3schools.com/w3images/gondol.jpg',
+        title:'Ipsum',
+        desc:'Praes tinci sed'
+    },
+    {
+        img:'https://www.w3schools.com/w3images/skies.jpg',
+        title:'Dorum',
+        desc:'Ultricies congue'
+    },
+    {
+        img:'https://www.w3schools.com/w3images/rock.jpg',
+        title:'Mingsum',
+        desc:'Lorem ipsum dipsum'
+    }
+]
 
 const IntroductionMenu = () =>{
     return(
        <div className="w3-col l4">
            <About/>
-           <PopularPosts/>
+           <Title/>
+           <PopularPosts
+               img ={popular_posts[0].img}
+               title ={popular_posts[0].title}
+               desc ={popular_posts[0].desc}
+           />
+           <PopularPosts
+               img ={popular_posts[1].img}
+               title ={popular_posts[1].title}
+               desc ={popular_posts[1].desc}
+           />
+           <PopularPosts
+               img ={popular_posts[2].img}
+               title ={popular_posts[2].title}
+               desc ={popular_posts[2].desc}
+           />
+           <PopularPosts
+               img ={popular_posts[3].img}
+               title ={popular_posts[3].title}
+               desc ={popular_posts[3].desc}
+           />
            <Tags/>
        </div>
-    )
+    );
 }
 
 export default IntroductionMenu;
---
+++ src/components/Title.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Title = () => {
+    return (
+            <div className="w3-container w3-padding">
+                <h4>Popular Posts</h4>
+            </div>
+    );
+}
+
+export default Title;
